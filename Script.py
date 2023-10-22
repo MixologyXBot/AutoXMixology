02866866edc95a0c1f6c98971d088166f9dd56af
MixologyXBot <146635284+MixologyXBot@users.noreply.github.com>
2023-10-22 09:17:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """<b>Hii {},</b>\n\n
-This is Auto Filter Bot. I can provide Movies/Series/Animes , Search in our <b>@MixologyMoviesSeries</b> Group and Download ⚡"""
+This is Auto Filter Bot. I can provide Movies/Series/Animes, Search in our <b>@MixologyMoviesSeries</b> Group and Download ⚡"""
 
     HELP_TXT = """<b>Hey {}
 Here Is The Help For My Commands.</b>"""
