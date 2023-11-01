8e88c823120c8a45a23eb8833470a49b288a7e52
MixologyXBot <146635284+MixologyXBot@users.noreply.github.com>
2023-11-01 07:02:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -82,9 +82,7 @@ async def pm_text(bot, message):
     )
     await bot.send_message(
         chat_id=LOG_CHANNEL,
-        text=f"#PM_MSG\n\nName: {user} 
-                ID: {user_id} 
-                <b>Message: {content}</b>"
+        text=f"#PM_MSG\n\nName: {user}\n\nID: {user_id}\n\n<b>Message: {content}</b>"
     )
 
 @Client.on_callback_query(filters.regex(r"^next"))
