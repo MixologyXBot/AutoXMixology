c56dd75da530e3a244be2c6d870d6fac75a24e96
MixologyXBot <146635284+MixologyXBot@users.noreply.github.com>
2023-11-26 10:12:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001894730349')
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002007077094')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
+NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
